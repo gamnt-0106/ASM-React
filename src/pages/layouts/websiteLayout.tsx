fd5693ace76756a74e2f8bed0c7dcfe7b164c6a3
gamnt-0106 <gamntph14216@fpt.edu.vn>
2022-04-03 02:03:46 +0700

    first commit

---
+++ src/pages/layouts/websiteLayout.tsx
@@ -0,0 +1,21 @@
+
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Header from '../../components/Header'
+type Props = {}
+
+const WebsiteLayout = (props: Props) => {
+  return (
+    <div>
+         <header>
+            <Header/>
+          </header>
+        <main>
+           <Outlet />
+        </main>
+     
+    </div>
+  )
+}
+
+export default WebsiteLayout
